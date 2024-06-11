75f734963d50c979531c6621a65eb2a0ce2a31a2
albaLong1 <longiyts@ad.unc.edu>
2024-06-11 12:12:14 -0400

    Implemented TextNode class and updated import paths

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from textnode import TextNode
+
+def main():
+    dummy_value = TextNode("This is a text node","bold" , "https://www.boot.dev")
+    print(dummy_value)
+
+
+if __name__ == "__main__":
+    main()
